c3468f68df23e874739f40eea1289d16ef299533
ChetanSaraswat <chetan.1150@zenmonk.tech>
2025-12-11 12:01:31 +0530

    Fix: Update UUID generation method in customer model and migration; enhance seeder to include UUIDs for new entries

--- migrations/20251202090064-create-customer.js
+++ migrations/20251202090064-create-customer.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       uuid: {
         type: Sequelize.UUID,
-        defaultValue:  Sequelize.literal("uuid_generate_v4()"),
+        defaultValue: Sequelize.UUIDV4,
         allowNull: false,
         unique: true,
       },
--- models/customer/customer.model.js
+++ models/customer/customer.model.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       uuid: {
         type: DataTypes.UUID,
-        defaultValue: Sequelize.literal("uuid_generate_v4()"),
+        defaultValue: DataTypes.UUIDV4,
         unique: true,
         allowNull: false,
       },
