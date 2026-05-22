35df8059864976af0e17baadc4f300ddc68ce55d
anoop <anoop.1179@zenmonk.tech>
2026-05-22 18:49:32 +0530

    Feature: Add SSL support for PostgreSQL in development configuration

--- config/db/config.js
+++ config/db/config.js
@@ -31,6 +31,12 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     seederStorage: "sequelize",
     logging: false,
     define: {
