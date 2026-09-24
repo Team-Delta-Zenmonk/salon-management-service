f8ced0ff61517a519e23ae8e995e3c34b53fd2b8
Parmeet Kumar <parmeetkumarg90@gmail.com>
2026-09-24 17:55:06 +0530

    Feature: Refactor redis configuration

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,11 @@
 const redisConnection = {
   host: process.env.REDIS_HOST || "redis",
   port: Number(process.env.REDIS_PORT) || 6379,
-  maxRetriesPerRequest: null, 
+  username: process.env.REDIS_USERNAME || "default",
+  password: process.env.REDIS_PASSWORD || undefined,
+  tls: process.env.REDIS_TLS === "true" ? {} : undefined,
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
 };
 
 module.exports = { redisConnection };
